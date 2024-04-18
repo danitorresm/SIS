c6cddea0ad649d1248991e9d108aa8ad1b636b61
danitorresm <danitorresm734@gmail.com>
2024-04-18 02:02:14 -0500

    Guardar cambios en SIS.assets.cache

--- obj/Debug/net8.0/SIS.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c86328d9dcacb725015d6e172d9bc68a9eb59c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e99754ef6e08e862b79f596f3dbbc740aa098c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
